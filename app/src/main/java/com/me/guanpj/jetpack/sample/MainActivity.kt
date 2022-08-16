63b72d91854e463b4e13a31988df2511ceb2d473
guanpj <guanpj.me@gmail.com>
2022-08-16 19:27:23 +0800

    app module

--- app/src/main/java/com/me/guanpj/jetpack/sample/MainActivity.kt
+++ app/src/main/java/com/me/guanpj/jetpack/sample/MainActivity.kt
@@ -3,7 +3,9 @@ package com.me.guanpj.jetpack.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.alibaba.android.arouter.launcher.ARouter
 import com.me.guanpj.appbase.BaseActivity
+import com.me.guanpj.appbase.config.ARouterConfig
 import com.me.guanpj.jetpack.sample.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -15,7 +17,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     private fun initEvent() {
         mViewBinding.llAgency.setOnClickListener {
+            ARouter.getInstance().build(ARouterConfig.AGENCY_MESSAGE).withString("name", "guanpj").navigation()
+        }
 
+        mViewBinding.llRiskLevel.setOnClickListener {
+            ARouter.getInstance().build(ARouterConfig.RISK_LEVEL).navigation()
+        }
+
+        mViewBinding.llTravel.setOnClickListener {
+            ARouter.getInstance().build(ARouterConfig.TRAVEL_POLICY)
         }
     }
 
